a00cb083c0c875ad88fea7cbd86ae42535e252ba
Joao Martins <joaovitor.martins@gmail.com>
2015-03-11 23:17:29 -0300

    [RTC] Adding automatic connection between peers (server/client)

--- views/js/easyRTC.js
+++ views/js/easyRTC.js
@@ -4,18 +4,18 @@ var channelIsActive = {}; // tracks which channels are active
 
 $(function () {
     easyrtc.enableDebug(false);
-    easyrtc.enableDataChannels(true);
     easyrtc.enableVideo(false);
     easyrtc.enableAudio(false);
     easyrtc.enableVideoReceive(false);
     easyrtc.enableAudioReceive(false);
+
+    easyrtc.enableDataChannels(true);
 
     easyrtc.setDataChannelOpenListener(openListener);
     easyrtc.setDataChannelCloseListener(closeListener);
 
     easyrtc.setPeerListener(addToConversation);
 
-    easyrtc.setRoomOccupantListener(convertListToButtons);
     easyrtc.connect("easyrtc.dataMessaging", loginSuccess, loginFailure);
 });
 
@@ -27,67 +27,11 @@ function addToConversation(who, msgType, content) {
 
 function openListener(otherParty) {
     channelIsActive[otherParty] = true;
-    updateButtonState(otherParty);
 }
 
 function closeListener(otherParty) {
     channelIsActive[otherParty] = false;
-    updateButtonState(otherParty);
 }
-
-function convertListToButtons(roomName, occupantList, isPrimary) {
-    connectList = occupantList;
-
-    var otherClientDiv = document.getElementById('otherClients');
-    while (otherClientDiv.hasChildNodes()) {
-        otherClientDiv.removeChild(otherClientDiv.lastChild);
-    }
-
-    var label, button;
-    for (var easyrtcid in connectList) {
-        var rowGroup = document.createElement("span");
-        var rowLabel = document.createTextNode(easyrtc.idToName(easyrtcid));
-        rowGroup.appendChild(rowLabel);
-
-        button = document.createElement('button');
-        button.id = "connect_" + easyrtcid;
-        button.onclick = function(easyrtcid) {
-            return function() {
-                startCall(easyrtcid);
-            };
-        }(easyrtcid);
-        label = document.createTextNode("Connect");
-        button.appendChild(label);
-        rowGroup.appendChild(button);
-
-        button = document.createElement('button');
-        button.id = "send_" + easyrtcid;
-        button.onclick = function(easyrtcid) {
-            return function() {
-                sendStuffP2P(easyrtcid);
-            };
-        }(easyrtcid);
-        label = document.createTextNode("Send Message");
-        button.appendChild(label);
-        rowGroup.appendChild(button);
-        otherClientDiv.appendChild(rowGroup);
-        updateButtonState(easyrtcid);
-    }
-    if (!otherClientDiv.hasChildNodes()) {
-        otherClientDiv.innerHTML = "<em>Nobody else logged in to talk to...</em>";
-    }
-}
-
-function updateButtonState(otherEasyrtcid) {
-    var isConnected = channelIsActive[otherEasyrtcid];
-    if(document.getElementById('connect_' + otherEasyrtcid)) {
-        document.getElementById('connect_' + otherEasyrtcid).disabled = isConnected;
-    }
-    if( document.getElementById('send_' + otherEasyrtcid)) {
-        document.getElementById('send_' + otherEasyrtcid).disabled = !isConnected;
-    }
-}
-
 
 function startCall(otherEasyrtcid) {
     if (easyrtc.getConnectStatus(otherEasyrtcid) === easyrtc.NOT_CONNECTED) {
@@ -116,28 +60,9 @@ function startCall(otherEasyrtcid) {
     }
 }
 
-function sendStuffP2P(otherEasyrtcid) {
-    var text = document.getElementById('sendMessageText').value;
-    if (text.replace(/\s/g, "").length === 0) { // Don't send just whitespace
-        return;
-    }
-    if (easyrtc.getConnectStatus(otherEasyrtcid) === easyrtc.IS_CONNECTED) {
-        easyrtc.sendDataP2P(otherEasyrtcid, 'msg', text);
-    }
-    else {
-        easyrtc.showError("NOT-CONNECTED", "not connected to " + easyrtc.idToName(otherEasyrtcid) + " yet.");
-    }
-
-    addToConversation("Me", "msgtype", text);
-    document.getElementById('sendMessageText').value = "";
-}
-
-
 function loginSuccess(easyrtcid) {
     selfEasyrtcid = easyrtcid;
-    document.getElementById("iam").innerHTML = "I am " + easyrtcid;
 }
-
 
 function loginFailure(errorCode, message) {
     easyrtc.showError(errorCode, "failure to login");
