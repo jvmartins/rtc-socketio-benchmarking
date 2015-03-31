10309ba3f4ce6f0cb059e293da975e639a4ee590
Luis Custodio <luis.custodio@gmail.com>
2015-04-01 00:17:44 +0200

    Changing the port heroku uses to run the server

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
     res.render('/views/index.html')
 });
 
-var server = app.listen(3000, function () {
+var server = app.listen(process.env.PORT || 3000, function () {
   var host = server.address().address;
   var port = server.address().port;
   console.log('App listening at http://%s:%s', host, port);
